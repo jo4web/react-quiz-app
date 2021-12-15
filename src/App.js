2bb6fc6565c99c207a325dfda289ab83eb7968c3
Joelton Martins <joeltonmartins@yahoo.com>
2021-12-15 16:08:05 -0300

    fetch data from api and delete old data test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react"
 import Questions from "./Components/Questions"
 
 export default function App() {
+
+const [data, setData] = React.useState([])
+
+React.useEffect( () => {
+  fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  .then(res => res.json())
+    .then(data => {
+      const newData = JSON.parse(JSON.stringify(data.results))
+      setData(newData)
+    }
+    )
+}, [])
+
   
   return (
         <main>
-            <div>
-              Teste
-              </div>
-              <Questions />
+              <Questions data={data}/>
             </main>
   )
 } 
