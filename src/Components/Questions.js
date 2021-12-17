8ebce64da1ac476a9313c4ac2ea6bc80094f7655
Joelton Martins <joeltonmartins@yahoo.com>
2021-12-17 16:09:40 -0300

    update loop to get object

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -3,17 +3,16 @@ import "./Questions.css"
 
 export default function Questions({ data }) {
              
-    console.log(data)
 
     const QaElements = data.map(elements => {
+
         return (
                 <div className="container">
                         <h1 className="title-question">{elements.question} </h1>
                         <div className="answers">
-                            <div className="select-answers">{elements.incorrect_answers[0]}</div>
-                            <div className="select-answers">{elements.correct_answer}</div>
-                            <div className="select-answers">{elements.incorrect_answers[1]}</div>
-                            <div className="select-answers">{elements.incorrect_answers[2]}</div>
+                            {elements.answers.map(answers => {
+                                return (<div className="select-answers">{answers.select}</div>)
+                            })}
                         </div>
 
                     </div>
