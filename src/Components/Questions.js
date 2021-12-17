1480919f3140cd60e4937bc3f2bc845b651aa85c
Joelton Martins <joeltonmartins@yahoo.com>
2021-12-17 17:27:37 -0300

    insert the onClick function

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,17 +1,26 @@
 import React from "react"
 import "./Questions.css"
 
-export default function Questions({ data }) {
+export default function Questions({ data, findNanoId}) {
              
 
     const QaElements = data.map(elements => {
 
         return (
                 <div className="container">
+                    
                         <h1 className="title-question">{elements.question} </h1>
                         <div className="answers">
                             {elements.answers.map(answers => {
-                                return (<div className="select-answers">{answers.select}</div>)
+                                const style = {
+                                    backgroundColor: answers.isHeld ? "blue" : ""
+                                }
+                                
+                                return (
+                                        <div onClick={() => findNanoId(answers.nanoId)} className="select-answers" style={style}>
+                                            {answers.select}
+                                        </div>
+                                        )
                             })}
                         </div>
 
